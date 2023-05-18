2367801a03c20854cb9ee070e7148a32dc0a480c
Serhii Voznyi <serhiivoznyi@gmail.com>
2023-05-18 15:03:45 +0200

    Removed legacy code

---
+++ src/Ticketmaster.Discovery.Tests/DiscoveryApi.Unit.Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Ticketmaster.Discovery.Tests
+{
+    public class DiscoveryApiTests
+    {
+        [Fact]
+        public void Should_Construct()
+        {
+            var client = new DiscoveryApi("");
+        }
+    }
+}
